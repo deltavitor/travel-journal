4bb7c109bc853b81848884e45823a49e83169b0b
deltavitor <cvitor1618@gmail.com>
2022-02-14 10:12:16 -0300

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 
+import Header from "./components/Header.js"
+
 function App() {
   return(
     <div>
-      <h1>Hello</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import WorldIcon from "../images/world-icon.svg"
+
+function Header() {
+  return (
+    <header>
+      <img src={WorldIcon} />
+      <h1>my travel journal.</h1>
+    </header>
+  )
+}
+
+export default Header
